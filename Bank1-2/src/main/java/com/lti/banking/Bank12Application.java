7ece5d9f09e1e4899fdd45eb590342f0412e5551
asohan96 <47651619+asohan96@users.noreply.github.com>
2019-03-08 19:01:31 +0530

    Add files via upload
    Bank-1-2

---
+++ Bank1-2/src/main/java/com/lti/banking/Bank12Application.java
@@ -0,0 +1,14 @@
+package com.lti.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bank12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bank12Application.class, args);
+	}
+
+}
+
